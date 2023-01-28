195d93d3ffdb468aa27ae324430ec5e9144ad96d
Helder Silva <helder198922@gmail.com>
2023-01-28 12:12:12 -0300

    Inicio projeto conversor de moedas

---
+++ conversor-de-moedas-objetos/convert.py
@@ -0,0 +1,5 @@
+api_key = ""
+# a api é um dado sensível, pois se a gente for pagar por ela no caso, se for gratuita que é o nosso
+# caso está tudo bem. Já é projetos comerciais não é bom expor a api
+
+

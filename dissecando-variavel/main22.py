871cb2049d7c93bb275f7522d15749e305e48db5
Helder Silva <helder198922@gmail.com>
2023-07-16 11:35:47 -0300

    Dissecando uma Variável

---
+++ dissecando-variavel/main22.py
@@ -0,0 +1,12 @@
+a = input("Digite um valor: ")
+print("O tipo primitivo desse valor é", type(a))
+print("É númerico?",a.isnumeric())
+print("Só tem espaços?", a.isspace())
+print("É decimal?", a.isdecimal())
+print("É um título?", a.istitle())
+print("É capitalizado?", a.capitalize())
+print("É Alfanúmerico?", a.isalnum())
+print("Está em letra Maiuscula?", a.isupper())
+print("Está em Minuscula?", a.islower())
+
+

0e2d6459b0e2affef4c6b827aed3e5f99a6e6d8c
Helder Silva <helder198922@gmail.com>
2023-07-17 18:12:40 -0300

    Script do programa

---
+++ antecessor-sucessor/main21.py
@@ -0,0 +1,13 @@
+s1 = int(input('Digite um número, valor: '))
+
+antecessor = s1 - 1
+
+sucessor = s1 + 1
+
+print('O antecessor de', s1, 'é', antecessor)
+
+print('O sucessor de', s1,'é', sucessor)
+
+
+
+

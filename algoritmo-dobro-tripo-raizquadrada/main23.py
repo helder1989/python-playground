6ed275e60927daed87073eb990e86ded381a465e
Helder Silva <helder198922@gmail.com>
2023-07-18 14:06:07 -0300

    Código do programa

---
+++ algoritmo-dobro-tripo-raizquadrada/main23.py
@@ -0,0 +1,24 @@
+import math
+
+# Interagindo com o usuário
+numero = int(input("Registre um número: "))
+
+# Calculando o dobro
+
+dobro = numero * 2
+
+# Calculando o triplo
+
+triplo = numero * 3
+
+
+# Calculando a raiz quadrada
+
+raiz_quadrada = math.sqrt(numero)
+
+# Exibindo os resultados
+
+print("O dobro do número é:", dobro)
+print(" O tripo do número é:", triplo)
+print("A raiz quadrada do número é:", raiz_quadrada)
+
